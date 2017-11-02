02117249fcc7d220fc5cc8631098ddf872313563
simadad <simadad@sina.com>
2017-11-02 17:23:20 +0800

    ..

--- pro_weibo/views.py
+++ pro_weibo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from weibo.models import WBUser
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    users = WBUser.objects.all().order_by('-id')[:10]
+    return render(request, 'index.html', {
+        'users': users
+    })
