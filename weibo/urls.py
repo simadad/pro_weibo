7b6d79cc4af522cfdc1f7704cd25e1f4eb704fba
simadad <simadad@sina.com>
2017-11-06 11:30:15 +0800

    weibo update func_base

--- weibo/urls.py
+++ weibo/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from weibo import views
 
 urlpatterns = [
-    # url(r'^$', views.homepage, name='homepage'),
-    url(r'^$', views.HomePageView.as_view(), name='homepage'),
-    # url(r'^u', views.user_page, name='upage'),
-    url(r'^update', views.WBUpdate.as_view(), name='update'),
-    url(r'^u', views.UserPageView.as_view(), name='upage'),
+    url(r'^$', views.homepage, name='homepage'),
+    # url(r'^$', views.HomePageView.as_view(), name='homepage'),
+    url(r'^update', views.wb_update, name='update'),
+    # url(r'^update', views.WBUpdate.as_view(), name='update'),
+    url(r'^u', views.user_page, name='upage'),
+    # url(r'^u', views.UserPageView.as_view(), name='upage'),
 ]
--- weibo/views.py
+++ weibo/views.py
@@ -1,57 +1,65 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views import View
+# from django.views import View
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+# from django.utils.decorators import method_decorator
 from weibo.models import *
 # Create your views here.
 
 
-@method_decorator(login_required, 'dispatch')
-class HomePageView(View):
-    def get(self, request):
-        wb_user = get_object_or_404(WBUser, id=request.user.id)
-        # wbs = WeiBo.objects.all().order_by('-time_create')
-        return render(request, 'weibo/homepage.html', {
-            'wb_user': wb_user,
-            # 'wbs': wbs
-        })
+# @method_decorator(login_required, 'dispatch')
+# class HomePageView(View):
+#     def get(self, request):
+#         wb_user = get_object_or_404(WBUser, id=request.user.id)
+#         # wbs = WeiBo.objects.all().order_by('-time_create')
+#         return render(request, 'weibo/homepage.html', {
+#             'wb_user': wb_user,
+#             # 'wbs': wbs
+#         })
 
 
-# @login_required
-# def homepage(request):
-#     wb_user = get_object_or_404(WBUser, id=request.user.id)
-#     # wbs = WeiBo.objects.all().order_by('-time_create')
-#     return render(request, 'weibo/homepage.html', {
-#         'wb_user': wb_user,
-#         # 'wbs': wbs
-#     })
+@login_required
+def homepage(request):
+    wb_user = get_object_or_404(WBUser, id=request.user.id)
+    # wbs = WeiBo.objects.all().order_by('-time_create')
+    return render(request, 'weibo/homepage.html', {
+        'wb_user': wb_user,
+        # 'wbs': wbs
+    })
 
 
-class UserPageView(View):
-    def get(self, request):
-        uid = request.GET.get('uid')
-        wb_user = get_object_or_404(WBUser, id=uid)
-        wbs = WeiBo.objects.filter(user=wb_user).order_by('-time_create')
-        return render(request, 'weibo/user_page.html', {
-            'wb_user': wb_user,
-            'wbs': wbs
-        })
+# class UserPageView(View):
+#     def get(self, request):
+#         uid = request.GET.get('uid')
+#         wb_user = get_object_or_404(WBUser, id=uid)
+#         wbs = WeiBo.objects.filter(user=wb_user).order_by('-time_create')
+#         return render(request, 'weibo/user_page.html', {
+#             'wb_user': wb_user,
+#             'wbs': wbs
+#         })
 
 
-# def user_page(request):
-#     uid = request.GET.get('uid')
-#     wb_user = get_object_or_404(WBUser, id=uid)
-#     wbs = WeiBo.objects.filter(user=wb_user).order_by('-time_create')
-#     return render(request, 'weibo/user_page.html', {
-#         'wb_user': wb_user,
-#         'wbs': wbs
-#     })
+def user_page(request):
+    uid = request.GET.get('uid')
+    wb_user = get_object_or_404(WBUser, id=uid)
+    wbs = WeiBo.objects.filter(user=wb_user).order_by('-time_create')
+    return render(request, 'weibo/user_page.html', {
+        'wb_user': wb_user,
+        'wbs': wbs
+    })
 
 
-class WBUpdate(View):
-    def post(self, request):
-        wb_user = get_object_or_404(WBUser, id=request.user.id)
-        msg = request.POST.get('msg')
-        wb = wb_user.update(msg)
-        return HttpResponse(render(request, 'weibo/new_wb.html', {'wb': wb}))
+# class WBUpdate(View):
+#     def post(self, request):
+#         wb_user = get_object_or_404(WBUser, id=request.user.id)
+#         msg = request.POST.get('msg')
+#         wb = wb_user.update(msg)
+#         return HttpResponse(render(request, 'weibo/new_wb.html', {'wb': wb}))
+
+
+def wb_update(request):
+    print(request.user.id)
+    wb_user = get_object_or_404(WBUser, id=request.user.id)
+    msg = request.POST.get('msg')
+    wb = wb_user.update(msg)
+    return HttpResponse(render(request, 'weibo/new_wb.html', {'wb': wb}))
